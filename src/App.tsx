fe06ff2748f053b859ba62e4b85f1d2bbac3bce8
Pascal Klodel <99654847+KiroLakestrike@users.noreply.github.com>
2026-03-14 14:07:45 +0100

    feat: Add new card layout and improve ShareToast styles and functionality

--- src/App.tsx
+++ src/App.tsx
@@ -6,25 +6,28 @@ import {ToastProvider} from "./components/context/ToastContext.tsx";
 
 function App() {
 
-  return (
-    <>
-      <ToastProvider>
-      <div className="card-container">
-        <img src="/images/drawers.jpg" alt="Green Drawers" />
-        <div className="card-content">
-          <h1>Green Drawers</h1>
-          <p>A collection of handcrafted, eco-friendly drawers.</p>
-          <div className="author-info">
-
-            <ProfileAndDate image={"/images/avatar-michelle.jpg"} name={"Michelle Appleton"} date={"28 Jun 2020"} />
-            <ShareButton />
-          </div>
-          <ShareToast></ShareToast>
-        </div>
-      </div>
-      </ToastProvider>
-    </>
-  )
+    return (
+        <>
+            <ToastProvider>
+                <main>
+                    <div className="card-container">
+                        <img src="/images/drawers.jpg" alt="Green Drawers" />
+                        <div className="card-content">
+                            <h1 className={"manrope-Bold text-color-grey-900 text-size-20 letter-spacing-025"}>Shift the overall look and feel by adding these wonderful touches to furniture in your home</h1>
+                            <p className={"manrope-Medium text-color-grey-500 text-size-13 letter-spacing-012"}>Ever been in a room and felt like something was missing? Perhaps it felt slightly bare and uninviting. I’ve got some simple tips to help you make any room feel complete.</p>
+                            <div className="share-section">
+                                <div className="author-info">
+                                    <ProfileAndDate image={"/images/avatar-michelle.jpg"} name={"Michelle Appleton"} date={"28 Jun 2020"} />
+                                    <ShareButton />
+                                </div>
+                                <ShareToast />
+                            </div>
+                        </div>
+                    </div>
+                </main>
+            </ToastProvider>
+        </>
+    )
 }
 
 export default App

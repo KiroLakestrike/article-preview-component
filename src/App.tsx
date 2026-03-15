bda2c70103b33b06007f2c85e9c56b3b3d83ee67
Pascal Klodel <99654847+KiroLakestrike@users.noreply.github.com>
2026-03-15 09:31:49 +0100

    refactor: Simplify image imports and improve component props in ProfileAndDate and App

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import ShareButton from "./components/ShareButton.tsx";
 import ProfileAndDate from "./components/ProfileAndDate.tsx";
 import ShareToast from "./components/ShareToast.tsx";
 import {ToastProvider} from "./components/context/ToastContext.tsx";
+import drawersImg from '../public/images/drawers.jpg'
+
 
 function App() {
 
@@ -11,13 +13,13 @@ function App() {
             <ToastProvider>
                 <main>
                     <div className="card-container">
-                        <img src="/images/drawers.jpg" alt="Green Drawers" />
+                        <img src={drawersImg} alt="Green Drawers" />
                         <div className="card-content">
                             <h1 className={"manrope-Bold text-color-grey-900 text-size-20 letter-spacing-025"}>Shift the overall look and feel by adding these wonderful touches to furniture in your home</h1>
                             <p className={"manrope-Medium text-color-grey-500 text-size-13 letter-spacing-012"}>Ever been in a room and felt like something was missing? Perhaps it felt slightly bare and uninviting. I’ve got some simple tips to help you make any room feel complete.</p>
                             <div className="share-section">
                                 <div className="author-info">
-                                    <ProfileAndDate image={"/images/avatar-michelle.jpg"} name={"Michelle Appleton"} date={"28 Jun 2020"} />
+                                    <ProfileAndDate name={"Michelle Appleton"} date={"28 Jun 2020"} />
                                     <div className="share-anchor">
                                         <ShareButton />
                                         <ShareToast />
--- src/components/ProfileAndDate.tsx
+++ src/components/ProfileAndDate.tsx
@@ -1,17 +1,17 @@
 import './ProfileAndDate.css'
+import avatarMichelle from '/public/images/avatar-michelle.jpg'
 
 
 interface ProfileAndDateProps {
-    image: string;
     name: string;
     date: string;
 }
 
-export default function ProfileAndDate({image, name, date}: ProfileAndDateProps) {
+export default function ProfileAndDate({ name, date}: ProfileAndDateProps) {
 
     return (
         <div className="profile-and-date-container">
-            <img src={image} alt={name} />
+            <img src={avatarMichelle} alt={name} />
             <div className="profile-and-date-info">
                 <h2 className={"manrope-Medium text-size-13 letter-spacing-012 text-color-grey-900"}>{name}</h2>
                 <p className={"manrope-Regular text-size-13 letter-spacing-012 text-color-grey-400"}>{date}</p>
